117211c9f19cae837649e4b5966fcef018b98cbe
[Lewis Kori] <[korilewis958@gmail.com]>
2024-04-17 12:07:58 +0300

    servo_control

---
+++ web2/web2.ino
@@ -0,0 +1,76 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-controls-servo-motor-via-web
+ */
+
+#include <Servo.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSocketsServer.h>
+#include "index.h"
+
+#define SERVO_PIN 15 // ESP32 pin GPIO26 connected to servo motor
+
+Servo servo;
+
+const char* ssid = "Kori";     // CHANGE IT
+const char* password = "YOUR_WIFI_PASSWORD";  // CHANGE IT
+
+AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);  // WebSocket server on port 81
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+      }
+      break;
+    case WStype_TEXT:
+      //Serial.printf("[%u] Received text: %s\n", num, payload);
+      String angle = String((char*)payload);
+      int angle_value = angle.toInt();
+      Serial.println(angle_value);
+      servo.write(angle_value);
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  servo.attach(SERVO_PIN);  // attaches the servo on ESP32 pin
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Initialize WebSocket server
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+  // Serve a basic HTML page with JavaScript to create the WebSocket connection
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    Serial.println("Web Server: received a web page request");
+    String html = HTML_CONTENT;  // Use the HTML content from the index.h file
+    request->send(200, "text/html", html);
+  });
+
+  server.begin();
+  Serial.print("ESP32 Web Server's IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  webSocket.loop();
+}
